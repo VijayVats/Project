6d3bf213ee306ab357b35b9be6d79d527cb269bf
VijayVats <101328839+VijayVats@users.noreply.github.com>
2025-12-16 12:10:57 +0530

    Add files via upload

---
+++ studentData.js
@@ -0,0 +1,674 @@
+ const students = [
+  {
+    "enrollment_no": "2353114902",
+    "name": "Tanisha Dev",
+    "email": "tanisha.dev10b@gmail.com",
+    "project_title": "Online Event Booking System",
+    "guide": "Vijay",
+    "co_guide": "",
+    "guide_email": "vijayvats@cftedu.in",
+    "co_guide_email": "",
+    "status": "confirm"
+  },
+  {
+    "enrollment_no": "2351765280",
+    "name": "Mohd Areeb",
+    "email": "areebmohd501@gmail.com",
+    "project_title": "LearnXGrow – An Online Learning Platform",
+    "guide": "Vinay Aggarwal Sir",
+    "co_guide": "",
+    "guide_email": "vinay@cftedu.in",
+    "co_guide_email": "",
+    "status": "confirm"
+  },
+  {
+    "enrollment_no": "2350622849",
+    "name": "Ekta Mishra",
+    "email": "ektam7455@gmail.com",
+    "project_title": "Mishra pooja Bhandar",
+    "guide": "Vijay",
+    "co_guide": "",
+    "guide_email": "vijayvats@cftedu.in",
+    "co_guide_email": "",
+    "status": "confirm"
+  },
+  {
+    "enrollment_no": "2351947535",
+    "name": "Deepshikha Tomar",
+    "email": "tomardeepshikha04@gmail.com",
+    "project_title": "Sync flex",
+    "guide": "Satya Prakash",
+    "co_guide": "Vijay",
+    "guide_email": "sprakash@pctiltd.com",
+    "co_guide_email": "vijayvats@cftedu.in",
+    "status": "confirm"
+  },
+  {
+    "enrollment_no": "2353015200",
+    "name": "ARPITA SRIVASTAV",
+    "email": "arpitasrivastav05@gmail.com",
+    "project_title": "Naari Heritage",
+    "guide": "Vijay",
+    "co_guide": "",
+    "guide_email": "vijayvats@cftedu.in",
+    "co_guide_email": "",
+    "status": "confirm"
+  },
+  {
+    "enrollment_no": "2351271570",
+    "name": "Sloni Raj",
+    "email": "sloniraj2004@gmail.com",
+    "project_title": "Chatpata Junction",
+    "guide": "Satya Prakash",
+    "co_guide": "Akash",
+    "guide_email": "sprakash@pctiltd.com",
+    "co_guide_email": "akash@cftedu.in",
+    "status": "confirm"
+  },
+  {
+    "enrollment_no": "2351366567",
+    "name": "Riturani Verma",
+    "email": "rituraniverma20@gmail.com",
+    "project_title": "Aura",
+    "guide": "Devendra Sir",
+    "co_guide": "Sachin",
+    "guide_email": "devendra@pctiltd.com",
+    "co_guide_email": "sachin@cftedu.in",
+    "status": "confirm"
+  },
+  {
+    "enrollment_no": "2350577523",
+    "name": "Nazrin",
+    "email": "nazreen.2605@gmail.com",
+    "project_title": "Calorie Tracker",
+    "guide": "Satya Prakash",
+    "co_guide": "Vijay",
+    "guide_email": "sprakash@pctiltd.com",
+    "co_guide_email": "vijayvats@cftedu.in",
+    "status": "confirm"
+  },
+  {
+    "enrollment_no": "2351110425",
+    "name": "Mohd Zaid",
+    "email": "zaidm1323@gmail.com",
+    "project_title": "TrendyKart",
+    "guide": "Devendra Sir",
+    "co_guide": "Sachin",
+    "guide_email": "devendra@pctiltd.com",
+    "co_guide_email": "sachin@cftedu.in",
+    "status": "confirm"
+  },
+  {
+    "enrollment_no": "2350580023",
+    "name": "Aayushi Prasad",
+    "email": "gungunkumari52@gmail.com",
+    "project_title": "Stylish Zone",
+    "guide": "Vijay",
+    "co_guide": "",
+    "guide_email": "vijayvats@cftedu.in",
+    "co_guide_email": "",
+    "status": "confirm"
+  },
+  {
+    "enrollment_no": "2350672963",
+    "name": "Shivam Kumar",
+    "email": "shivamkumara840@gmail.com",
+    "project_title": "Fitness Buddies",
+    "guide": "Bhanu Pratap Sir",
+    "co_guide": "Harshita",
+    "guide_email": "bhanu@cftedu.in",
+    "co_guide_email": "harshita@cftedu.in",
+    "status": "confirm"
+  },
+  {
+    "enrollment_no": "2350630696",
+    "name": "Neetu",
+    "email": "vishal971146@gmail.com",
+    "project_title": "Daily Expenses Tracker System (Web-based)",
+    "guide": "Bhanu Pratap Sir",
+    "co_guide": "Prerna",
+    "guide_email": "bhanu@cftedu.in",
+    "co_guide_email": "prerna@cftedu.in",
+    "status": "confirm"
+  },
+  {
+    "enrollment_no": "2353612593",
+    "name": "Vishal Kumar Raut",
+    "email": "vishalraut7547@gmail.com",
+    "project_title": "HisaabKitab",
+    "guide": "Devendra Sir",
+    "co_guide": "Harshita",
+    "guide_email": "devendra@pctiltd.com",
+    "co_guide_email": "harshita@cftedu.in",
+    "status": "confirm"
+  },
+  {
+    "enrollment_no": "2251078320",
+    "name": "Gunjan",
+    "email": "gunjantejan@gmail.com",
+    "project_title": "E-Shopnex",
+    "guide": "Bhanu Pratap Sir",
+    "co_guide": "Prerna",
+    "guide_email": "bhanu@cftedu.in",
+    "co_guide_email": "prerna@cftedu.in",
+    "status": "confirm"
+  },
+  {
+    "enrollment_no": "2352842694",
+    "name": "Muskan Sharma",
+    "email": "muskanhariom56@gmail.com",
+    "project_title": "Online Book Management System for Shop",
+    "guide": "Satya Prakash",
+    "co_guide": "Akash",
+    "guide_email": "sprakash@pctiltd.com",
+    "co_guide_email": "akash@cftedu.in",
+    "status": "confirm"
+  },
+  {
+    "enrollment_no": "2350664905",
+    "name": "Nancy Jain",
+    "email": "2004nancyjain@gmail.com",
+    "project_title": "Jz Learning Home",
+    "guide": "Vijay",
+    "co_guide": "",
+    "guide_email": "vijayvats@cftedu.in",
+    "co_guide_email": "",
+    "status": "confirm"
+  },
+  {
+    "enrollment_no": "2352793100",
+    "name": "Naina Verma",
+    "email": "4051naina@gmail.com",
+    "project_title": "Jobs Search Website",
+    "guide": "Vinay Aggarwal Sir",
+    "co_guide": "",
+    "guide_email": "vinay@cftedu.in",
+    "co_guide_email": "",
+    "status": "confirm"
+  },
+  {
+    "enrollment_no": "2351797369",
+    "name": "Archana Yadav",
+    "email": "archanayadav122004@gmail.com",
+    "project_title": "A Real-Time Chat Application",
+    "guide": "Satya Prakash",
+    "co_guide": "Prerna",
+    "guide_email": "sprakash@pctiltd.com",
+    "co_guide_email": "prerna@cftedu.in",
+    "status": "confirm"
+  },
+  {
+    "enrollment_no": "2352639115",
+    "name": "Anshika Kathait",
+    "email": "anshika.kathait700@gmail.com",
+    "project_title": "SkillSwap",
+    "guide": "Bhanu Pratap Sir",
+    "co_guide": "Sachin Sir",
+    "guide_email": "bhanu@cftedu.in",
+    "co_guide_email": "sachin@vetab.in",
+    "status": "confirm"
+  },
+  {
+    "enrollment_no": "2352683777",
+    "name": "Mohan Arora",
+    "email": "mohanarora987@gmail.com",
+    "project_title": "",
+    "guide": "Rahul",
+    "co_guide": "",
+    "guide_email": "rahul@cftedu.in",
+    "co_guide_email": "",
+    "status": "confirm"
+  },
+
+  {
+    "enrollment_no": "2351765593",
+    "name": "Aryan Jha",
+    "email": "aryan555.jha@gmail.com",
+    "project_title": "",
+    "guide": "Shiwangi Ma'am",
+    "co_guide": "Harshita",
+    "guide_email": "shiwangi@cftedu.in",
+    "co_guide_email": "harshita@cftedu.in",
+    "status": "confirm"
+  },
+  {
+    "enrollment_no": "2353701350",
+    "name": "Aryan",
+    "email": "aryanakki619@gmail.com",
+    "project_title": "Online Bookstore",
+    "guide": "Shiwangi Ma'am",
+    "co_guide": "Prerna",
+    "guide_email": "shiwangi@cftedu.in",
+    "co_guide_email": "prerna@cftedu.in",
+    "status": "confirm"
+  },
+  {
+    "enrollment_no": "2350562499",
+    "name": "Mridul Vats",
+    "email": "mridulvats71@gmail.com",
+    "project_title": "",
+    "guide": "Pardeep Sir",
+    "co_guide": "Akash Sir",
+    "guide_email": "pardeep@cftedu.in",
+    "co_guide_email": "akash@cftedu.in",
+    "status": "confirm"
+  },
+  {
+    "enrollment_no": "2352085667",
+    "name": "Abhay Jain",
+    "email": "abhay2jn@gmail.com",
+    "project_title": "Deliveryhub",
+    "guide": "Pardeep Sir",
+    "co_guide": "",
+    "guide_email": "pardeep@cftedu.in",
+    "co_guide_email": "",
+    "status": "confirm"
+  },
+  {
+    "enrollment_no": "2354232561",
+    "name": "Sambhav Jha",
+    "email": "sambhavjha2@gmail.com",
+    "project_title": "Hospital and Clinic Management System",
+    "guide": "Pardeep Sir",
+    "co_guide": "",
+    "guide_email": "pardeep@cftedu.in",
+    "co_guide_email": "",
+    "status": "confirm"
+  },
+  {
+    "enrollment_no": "2353375254",
+    "name": "Nikhil Goswami",
+    "email": "nikhilpandat2211@gmail.com",
+    "project_title": "Dailydeals Hub",
+    "guide": "Pardeep Sir",
+    "co_guide": "",
+    "guide_email": "pardeep@cftedu.in",
+    "co_guide_email": "",
+    "status": "confirm"
+  },
+  {
+    "enrollment_no": "2352851058",
+    "name": "Himanshu Prajapati",
+    "email": "hp545972@gmail.com",
+    "project_title": "ShopEase",
+    "guide": "Rahul",
+    "co_guide": "Sachin",
+    "guide_email": "rahul@cftedu.in",
+    "co_guide_email": "sachin@cftedu.in",
+    "status": "confirm"
+  },
+  {
+    "enrollment_no": "2353373673",
+    "name": "Ritik Raghav",
+    "email": "ritikkraghav@gmail.com",
+    "project_title": "Infinity Shopping",
+    "guide": "Shiwangi Ma'am",
+    "co_guide": "Akash",
+    "guide_email": "shiwangi@cftedu.in",
+    "co_guide_email": "akash@cftedu.in",
+    "status": "confirm"
+  },
+  {
+    "enrollment_no": "2351787571",
+    "name": "Chanda Khatoon",
+    "email": "chanda2468khatoon@gmail.com",
+    "project_title": "Hostel Visitor Management",
+    "guide": "Vinay Aggarwal Sir",
+    "co_guide": "",
+    "guide_email": "vinay@cftedu.in",
+    "co_guide_email": "",
+    "status": "confirm"
+  },
+  {
+    "enrollment_no": "2352088244",
+    "name": "Mayank Keshria",
+    "email": "mayankkeshria@gmail.com",
+    "project_title": "Arenax – The eSports Battleground Hub",
+    "guide": "Vinay Aggarwal Sir",
+    "co_guide": "",
+    "guide_email": "vinay@cftedu.in",
+    "co_guide_email": "",
+    "status": "confirm"
+  },
+  {
+    "enrollment_no": "2351108752",
+    "name": "Sameer Saifi",
+    "email": "sameersaifi4442@gmail.com",
+    "project_title": "Online Shopping System",
+    "guide": "Vinay Aggarwal Sir",
+    "co_guide": "",
+    "guide_email": "vinay@cftedu.in",
+    "co_guide_email": "",
+    "status": "confirm"
+  },
+  {
+    "enrollment_no": "2352848603",
+    "name": "Lokesh Kumar",
+    "email": "lokeshedu9@gmail.com",
+    "project_title": "Smart Campus Dining",
+    "guide": "Rahul",
+    "co_guide": "",
+    "guide_email": "rahul@cftedu.in",
+    "co_guide_email": "",
+    "status": "confirm"
+  },
+  {
+    "enrollment_no": "2351113790",
+    "name": "Anshu",
+    "email": "bipinkumar110084@gmail.com",
+    "project_title": "",
+    "guide": "Salim Sir",
+    "co_guide": "",
+    "guide_email": "salim@vetab.in",
+    "co_guide_email": "",
+    "status": "confirm"
+  },
+  {
+    "enrollment_no": "2350654743",
+    "name": "Raja Kumar",
+    "email": "rajakumar332323@gmail.com",
+    "project_title": "",
+    "guide": "Devendra Sir",
+    "co_guide": "Akash",
+    "guide_email": "devendra@pctiltd.com",
+    "co_guide_email": "akash@cftedu.in",
+    "status": "confirm"
+  },
+  {
+    "enrollment_no": "2350165280",
+    "name": "Tannu",
+    "email": "abhishekbaghel846@gmail.com",
+    "project_title": "",
+    "guide": "Shiwangi Ma'am",
+    "co_guide": "",
+    "guide_email": "shiwangi@cftedu.in",
+    "co_guide_email": "",
+    "status": "confirm"
+  },
+  {
+    "enrollment_no": "2353347268",
+    "name": "Rashmi Prasad",
+    "email": "dkprasad00234@gmail.com",
+    "project_title": "Cafetra Area",
+    "guide": "Salim Sir",
+    "co_guide": "",
+    "guide_email": "salim@vetab.in",
+    "co_guide_email": "",
+    "status": "confirm"
+  },
+  {
+    "enrollment_no": "2353612799",
+    "name": "Shafali",
+    "email": "shefalix85@gmail.com",
+    "project_title": "Institute Website",
+    "guide": "Satya Prakash",
+    "co_guide": "Salim Sir",
+    "guide_email": "sprakash@pctiltd.com",
+    "co_guide_email": "salim@vetab.in",
+    "status": "confirm"
+  },
+  {
+    "enrollment_no": "2351375234",
+    "name": "Yash Kumar Pal",
+    "email": "yashkumar280704@gmail.com",
+    "project_title": "Gaming Café",
+    "guide": "Bhanu Pratap Sir",
+    "co_guide": "Akash",
+    "guide_email": "bhanu@cftedu.in",
+    "co_guide_email": "akash@cftedu.in",
+    "status": "confirm"
+  },
+  {
+    "enrollment_no": "2350656941",
+    "name": "Ragini Thakran",
+    "email": "raginithakran@gmail.com",
+    "project_title": "",
+    "guide": "Bhanu Pratap Sir",
+    "co_guide": "Salim Sir",
+    "guide_email": "bhanu@cftedu.in",
+    "co_guide_email": "salim@vetab.in",
+    "status": "confirm"
+  },
+  {
+    "enrollment_no": "2352575999",
+    "name": "Saloni Jha",
+    "email": "sja749811@gmail.com",
+    "project_title": "JobHunt",
+    "guide": "Rahul",
+    "co_guide": "",
+    "guide_email": "rahul@cftedu.in",
+    "co_guide_email": "",
+    "status": "confirm"
+  },
+  {
+    "enrollment_no": "2353018576",
+    "name": "Jaishri Gusai",
+    "email": "jaishrigusai@gmail.com",
+    "project_title": "Fashion Fusion",
+    "guide": "Salim Sir",
+    "co_guide": "",
+    "guide_email": "salim@vetab.in",
+    "co_guide_email": "",
+    "status": "confirm"
+  },
+  {
+    "enrollment_no": "2350640307",
+    "name": "Kartik",
+    "email": "kartikrawat349@gmail.com",
+    "project_title": "",
+    "guide": "Salim Sir",
+    "co_guide": "",
+    "guide_email": "salim@vetab.in",
+    "co_guide_email": "",
+    "status": "confirm"
+  },
+  {
+    "enrollment_no": "2301115499",
+    "name": "Shivam Dixit",
+    "email": "dixitshivam591@gmail.com",
+    "project_title": "An Online Pharmacy",
+    "guide": "Salim Sir",
+    "co_guide": "",
+    "guide_email": "salim@vetab.in",
+    "co_guide_email": "",
+    "status": "confirm"
+  },
+  {
+    "enrollment_no": "2351114122",
+    "name": "Kuldeep Thakur",
+    "email": "kuldeep931124@gmail.com",
+    "project_title": "Once Beauty",
+    "guide": "Salim Sir",
+    "co_guide": "",
+    "guide_email": "salim@vetab.in",
+    "co_guide_email": "",
+    "status": "confirm"
+  },
+  {
+    "enrollment_no": "2253193467",
+    "name": "Himanshu Raj",
+    "email": "nxxbjile@gmail.com",
+    "project_title": "TryOnStore",
+    "guide": "Devendra Sir",
+    "co_guide": "Harshita",
+    "guide_email": "devendra@pctiltd.com",
+    "co_guide_email": "harshita@cftedu.in",
+    "status": "confirm"
+  },
+ {
+    "enrollment_no": "2352400795",
+    "name": "Priya",
+    "email": "priyashankhwar216@gmail.com",
+    "project_title": "TechMart",
+    "guide": "Bhanu Pratap Sir",
+    "co_guide": "akash",
+    "guide_email": "bhanu@cftedu.in",
+    "co-guide_email": "akash@cftedu.in",
+    "status": "confirm"
+  },
+  {
+    "enrollment_no": "2350563284",
+    "name": "Tushar Upreti",
+    "email": "tushar9912126@gmail.com",
+    "project_title": "UE Chat",
+    "guide": "Pardeep Sir",
+    "co-guide": "",
+    "guide_email": "pardeep@cftedu.in",
+    "co-guide_email": "",
+    "status": "confirm"
+  },
+  {
+    "enrollment_no": "2353161946",
+    "name": "Khushi Gautam",
+    "email": "dearkhushi2207@gmail.com",
+    "project_title": "BookSwap Hub",
+    "guide": "Satya Prakash",
+    "co-guide": "Prerna",
+    "guide_email": "sprakash@pctiltd.com",
+    "co-guide_email": "prerna@cftedu.in",
+    "status": "confirm"
+  },
+  {
+    "enrollment_no": "2351788259",
+    "name": "Krishna Khurana",
+    "Email": "krishnakhurana21022024@gmail.com",
+    "project_title": "Inventory Management System",
+    "guide": "Rahul",
+    "co-guide": "",
+    "guide_email": "rahul@cftedu.in",
+    "co-guide_email": "",
+    "status": "confirm"
+  },
+
+{
+    "enrollment_no": "2353620979",
+    "name": "Parul Sharma",
+    "Email": "sharmap81641@gmail.com",
+    "project_title": "",
+    "guide": "Pardeep Sir",
+    "co-guide": "Sachin Sir",
+    "guide_email": "pardeep@cftedu.in",
+    "co-guide_email": "sachin@cftedu.in",
+    "status": "confirm"
+  },
+{
+    "enrollment_no": "2350005765",
+    "name": "Naman Bansal",
+    "Email": "nnamanbanssal@gmail.com",
+    "project_title": "car Street",
+    "guide": "Vijay",
+    "co-guide": "",
+    "guide_email": "vijayvats@cftedu.in",
+    "co-guide_email": "",
+    "status": "confirm"
+  },
+
+
+ ];
+// ===== SEARCH FUNCTION =====
+const guideSelect = document.getElementById("guideSelect");
+const uniqueGuides = [...new Set(students.map(s => s.guide))];
+
+uniqueGuides.forEach(guide => {
+    const option = document.createElement("option");
+    option.value = guide;
+    option.textContent = guide;
+    guideSelect.appendChild(option);
+});
+// ===== SEARCH FUNCTION =====
+function searchGuide() {
+    const selectedGuide = guideSelect.value;
+    const tbody = document.querySelector("#resultTable tbody");
+    tbody.innerHTML = "";
+
+    if (!selectedGuide) {
+        alert("Please select a guide name");
+        return;
+    }
+
+    const filtered = students.filter(s => s.guide === selectedGuide);
+
+    document.getElementById("resultCount").innerText =
+        `Total Students under "${selectedGuide}": ${filtered.length}`;
+
+    filtered.forEach(s => {
+        const row = `<tr>
+            <td>${s.enrollment_no}</td>
+            <td>${s.name}</td>
+            <td>${s.email}</td>
+            <td>${s.project_title}</td>
+            <td>${s.guide}</td>
+	    <td>${s.co_guide}</td>
+        </tr>`;
+        tbody.innerHTML += row;
+    });
+
+    document.getElementById("resultTable").style.display = "table";
+}
+
+
+    function searchStudent() {
+        const input = document.getElementById("enrollInput").value.trim();
+        const resultBox = document.getElementById("result");
+        resultBox.innerHTML = "";
+
+        const student = students.find(s => s.enrollment === input);
+
+        if (student) {
+            resultBox.innerHTML = `
+                <div class="result-box">
+                    <p><strong>Student Name:</strong> ${student.name}</p>
+                    <p><strong>Project Title:</strong> ${student.projectTitle}</p>
+                    <p><strong>Status:</strong> ${student.status}</p>
+                    <p><strong>Guide Name:</strong> ${student.guideName}</p>
+                    <p><strong>Guide Email:</strong> ${student.guideEmail}</p>
+                    <p><strong>Co-Guide Name:</strong> ${student.coGuideName}</p>
+                    <p><strong>Co-Guide Email:</strong> ${student.coGuideEmail}</p>
+                </div>
+            `;
+        } else {
+            resultBox.innerHTML = `<p class='not-found'>No student found with this Enrollment Number!</p>`;
+        }
+    }
+
+// ===== POPULATE CO-GUIDE DROPDOWN =====
+const coGuideSelect = document.getElementById("coGuideSelect");
+const uniqueCoGuides = [...new Set(students.map(s => s.co_guide))];
+
+uniqueCoGuides.forEach(cg => {
+    const option = document.createElement("option");
+    option.value = cg;
+    option.textContent = cg;
+    coGuideSelect.appendChild(option);
+});
+
+// ===== SEARCH FUNCTION =====
+function searchCoGuide() {
+    const selected = coGuideSelect.value;
+    const tbody = document.querySelector("#resultTable tbody");
+    tbody.innerHTML = "";
+
+    if (!selected) {
+        alert("Please select a Co-Guide name");
+        return;
+    }
+
+    const filtered = students.filter(s => s.co_guide === selected);
+
+    document.getElementById("resultCount").innerText =
+        `Total Students under Co-Guide "${selected}": ${filtered.length}`;
+
+    filtered.forEach(s => {
+        const row = `<tr>
+            <td>${s.enrollment_no}</td>
+            <td>${s.name}</td>
+            <td>${s.email}</td>
+            <td>${s.project}</td>
+            <td>${s.guide}</td>
+            <td>${s.co_guide}</td>
+        </tr>`;
+        tbody.innerHTML += row;
+    });
+
+    document.getElementById("resultTable").style.display = "table";
+}
